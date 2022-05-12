f33a2e529a850cd3cea30b8eb6eb6fe3e115399c
Anthony <a.sabbagh@era2140.com>
2022-05-12 20:40:17 +0100

    feat: change restaurant

--- src/lib/socket-connect.js
+++ src/lib/socket-connect.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
-
+const RESTAURANT_ID = 'rstrnt00-0000-0000-0000-000000000002'
 function Socket (url) {
   const socket = io(url, { autoConnect: false })
   socket.onAny((event, ...args) => {
@@ -8,12 +8,12 @@ function Socket (url) {
   socket.connect()
   this.socket = socket
   this.getRestaurant = (cb) => {
-    this.socket.emit('on:restaurant', { uuid: 'rstrnt00-0000-0000-0000-000000000001' })
+    this.socket.emit('on:restaurant', { uuid: RESTAURANT_ID })
     this.socket.on('emit:restaurant', cb)
     return this.socket
   }
   this.getMenu = (cb) => {
-    this.socket.emit('on:menu:actual', { uuid: 'rstrnt00-0000-0000-0000-000000000001' })
+    this.socket.emit('on:menu:actual', { uuid: RESTAURANT_ID })
     this.socket.on('emit:menu:actual', cb)
     return this.socket
   }
